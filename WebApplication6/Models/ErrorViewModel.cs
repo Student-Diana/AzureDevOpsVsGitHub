06874dac499369eeb2160f6bb05be306573ab623
Diana Avanesian <dianaskybin@gmail.com>
2021-01-04 13:05:08 -0500

    Add project files.

---
+++ WebApplication6/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace WebApplication6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
